b9ad4abd2378526089b98b2d1ee2ef933f456eea
Lucas Henrique <lucasheriques@gmail.com>
2023-12-08 09:35:12 -0300

    another atempt

--- app/server.go
+++ app/server.go
@@ -1,10 +1,19 @@
 package main
 
+/**
+ * This is a simple Redis server. Our goal is to implement the Redis protocol.
+ */
+
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 )
+
+func getResponse(request string) string {
+	return "+PONG\r\n"
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -18,5 +27,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(conn, "+PONG\r\n")
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+
+	for {
+		request, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Printf("Received: %s\n", request)
+
+		response := getResponse(request)
+		_, err = writer.WriteString(response)
+		if err != nil {
+			fmt.Println("Error writing: ", err.Error())
+			os.Exit(1)
+		}
+		writer.Flush()
+	}
 }
