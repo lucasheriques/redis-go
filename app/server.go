9a35ec2e8a54e59918b47a7e625cbfdad0c02d03
Lucas Henrique <lucasheriques@gmail.com>
2023-12-08 10:08:45 -0300

    stage 4

--- app/server.go
+++ app/server.go
@@ -7,43 +7,46 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 )
 
-func getResponse(request string) string {
+func getResponse() string {
 	return "+PONG\r\n"
 }
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		request := scanner.Text()
+
+		log.Printf("Command recv: %s", request)
+		if strings.ToUpper(request) == "PING" {
+			conn.Write([]byte(getResponse()))
+		}
+	}
+}
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
-	reader := bufio.NewReader(conn)
-	writer := bufio.NewWriter(conn)
+	defer listener.Close()
 
 	for {
-		request, err := reader.ReadString('\n')
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error reading: ", err.Error())
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		fmt.Printf("Received: %s\n", request)
 
-		response := getResponse(request)
-		_, err = writer.WriteString(response)
-		if err != nil {
-			fmt.Println("Error writing: ", err.Error())
-			os.Exit(1)
-		}
-		writer.Flush()
+		handleConnection(conn)
 	}
 }
