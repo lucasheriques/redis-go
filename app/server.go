b2b2ba801ba783af14b86b3c64dadeaef76d6cfc
Lucas Henrique <lucasheriques@gmail.com>
2023-12-10 00:32:04 -0300

    stage 5

--- app/server.go
+++ app/server.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,16 +18,55 @@ func getResponse() string {
 	return "+PONG\r\n"
 }
 
+func executeCommand(command []string) string {
+	switch strings.ToUpper(command[0]) {
+	case "PING":
+		return "+PONG\r\n"
+	case "ECHO":
+		return fmt.Sprintf("$%d\r\n%s\r\n", len(command[1]), command[1])
+	}
+
+	return "-ERR unknown command '" + command[0] + "'\r\n"
+}
+
+func parseNextNLines(scanner *bufio.Scanner, n int) []string {
+	var lines []string
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		command := scanner.Text()
+
+		if command[0] == '$' {
+			_, err := strconv.Atoi(strings.Split(command, "$")[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			lines = append(lines, scanner.Text())
+		}
+	}
+	return lines
+}
+
 func handleConnection(conn net.Conn) {
-	defer conn.Close()
 	scanner := bufio.NewScanner(conn)
 
 	for scanner.Scan() {
 		request := scanner.Text()
 
-		log.Printf("Command recv: %s", request)
-		if strings.ToUpper(request) == "PING" {
-			conn.Write([]byte(getResponse()))
+		if request[0] == '*' {
+			numberOfLines, err := strconv.Atoi(strings.Split(request, "*")[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Number of lines: %d\n", numberOfLines)
+
+			command := parseNextNLines(scanner, numberOfLines*2)
+			log.Printf("Command recv: %s", command)
+
+			response := executeCommand(command)
+
+			conn.Write([]byte(response))
 		}
 	}
 }
@@ -42,6 +82,8 @@ func main() {
 
 	for {
 		conn, err := listener.Accept()
+		defer conn.Close()
+
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
